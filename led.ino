9ccc394a22d50df54ae2633473b69e1d551edf2c
mattmolo <molo.matt@gmail.com>
2015-09-10 13:02:53 -0400

    Actually added the blink and led files

---
+++ led.ino
@@ -0,0 +1,22 @@
+/*
+   Modify this code to control an LED with a potentiometer
+
+   Make sure to comment your code to explain what you're doing!
+*/
+
+// Put global variables here needed in setup and loop
+// globals will keep their state between the loop
+
+
+
+// the setup routine runs once when you press reset:
+// initialize pin outputs here, any other variables you need
+void setup() {                
+
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+
+}
